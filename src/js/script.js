7a4fd102748606ba716d92c9dbd921bfdad1e840
GuliMaster <43322103+GuliMaster@users.noreply.github.com>
2020-11-01 14:03:02 +0300

    front update
    zip/unzip requests

--- src/js/script.js
+++ src/js/script.js
@@ -7,36 +7,36 @@ function zipRequest(file) {
     }
     let formData = new FormData();
     formData.append('userfile', file);
-    xhr.open('POST', server + '/fileDown', true);
+    xhr.open('POST', server + '/zip', true);
     xhr.setRequestHeader("Accept","multipart/form-data");
     xhr.send(formData);
     xhr.onload = () => {
         document.getElementById('download').style.display = 'flex';
-        document.getElementById('download').firstElementChild.setAttribute('href', `${xhr.response}`);
+        document.getElementById('download').firstElementChild.setAttribute('href', `${server}` + '/downloads/' + `${xhr.response}`);
         document.getElementById('download').firstElementChild.setAttribute('download', '');
+        document.getElementById('status').textContent = '';
     }
     xhr.onerror = () => {
         document.getElementById('status').textContent = 'Ошибка соединения... Т_Т';
-    }
-    xhr.onprogress = (e) => {
-        document.getElementById('status').textContent = `Загрузка... ${e.loaded} из ${e.total}`;
     }
 }
 function unzipRequest(file) {
     if (!file) {
         return document.getElementById('status').textContent = 'Выберите файл! Пожалуйста... ♥';
     }
-    xhr.open('POST', server + '/decode', true);
-    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhr.send(`file=${file}`);
+    let formData = new FormData();
+    formData.append('userfile', file);
+    xhr.open('POST', server + '/unzip', true);
+    xhr.setRequestHeader("Accept","multipart/form-data");
+    xhr.send(formData);
     xhr.onload = () => {
-
+        document.getElementById('download').style.display = 'flex';
+        document.getElementById('download').firstElementChild.setAttribute('href', `${server}` + '/downloads/' + `${xhr.response}`);
+        document.getElementById('download').firstElementChild.setAttribute('download', '');
+        document.getElementById('status').textContent = '';
     }
     xhr.onerror = () => {
         document.getElementById('status').textContent = 'Ошибка соединения... Т_Т';
-    }
-    xhr.onprogress = (e) => {
-        document.getElementById('status').textContent = `Загрузка... ${e.loaded} из ${e.total}`;
     }
 }
 
